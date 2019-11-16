42cb7222e5025ec1df05563c320ec46042a5964a
Govind Vijay <gv.vj93@gmail.com>
2019-11-16 21:01:43 +0530

    Task4. Adding history support

--- controllers/token.js
+++ controllers/token.js
@@ -3,24 +3,47 @@ const path = require('path');
 
 exports.save = (req, res, next) => {
     const tokenPath = path.join(__dirname, '..', 'data', `token-${req.params.tokenType}.json`);
-    let success = true;
-    fs.writeFile(tokenPath, JSON.stringify(req.body), 'utf-8', err => {
-        if (err) {
-            success = false;
-            console.log(err);
+    fs.readFile(tokenPath, 'utf8', (err, data) => {
+        if(!data) {
+            data = '[]';
         }
+
+        let success = true;
+        const jsonData = JSON.stringify([req.body, ...JSON.parse(data)]);
+        fs.writeFile(tokenPath, jsonData, 'utf-8', err => {
+            if (err) {
+                success = false;
+                console.log(err);
+            }
+        });
+        res.json({success});
     });
-    res.json({success});
 };
 
 exports.fetch = (req, res, next) => {
     const tokenPath = path.join(__dirname, '..', 'data', `token-${req.params.tokenType}.json`);
     fs.readFile(tokenPath, 'utf8', (err, data) => {
         if(!data) {
-            data = '{}';
+            res.json('[]');
+            return;
         }
 
         const jsonData = JSON.parse(data);
+        const id = req.params.historyId === 'current' ? 0 : jsonData.findIndex(({historyId}) => {
+            return historyId === +req.params.historyId
+        });
+        res.json(jsonData[id]);
+    });
+};
+
+exports.fetchHistory = (req, res, next) => {
+    const tokenPath = path.join(__dirname, '..', 'data', `token-${req.params.tokenType}.json`);
+    fs.readFile(tokenPath, 'utf8', (err, data) => {
+        if(!data) {
+            data = '[]';
+        }
+
+        const jsonData = JSON.parse(data).map(({historyId}) => historyId);
         res.json(jsonData);
     });
 };
--- routes/token.js
+++ routes/token.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const {save, fetch} = require('../controllers/token');
+const {save, fetch, fetchHistory} = require('../controllers/token');
 
 const router = express.Router();
 
 router.post('/save/:tokenType', save);
-router.get('/fetch/:tokenType', fetch);
+router.get('/fetch/:tokenType/history', fetchHistory);
+router.get('/fetch/:tokenType/:historyId', fetch);
 
 module.exports = router;
